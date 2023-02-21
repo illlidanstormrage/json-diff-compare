c62fba26a78e28c86b63160af931857f29f5022f
illlidanstormrage <540089419@qq.com>
2023-02-21 22:17:06 +0800

    首页测试更改

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <DiffTree
+      :newData="newData"
+      :oldData="oldData"
+  ></DiffTree>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DiffTree from "@/components/DiffTree";
 
 export default {
   name: 'App',
+  data() {
+    return {
+      oldData: [
+        {
+          a: 'b',
+          b: 'c',
+        },
+        {
+          a: 'a',
+          b: 'b',
+        }
+      ],
+      newData: [
+        {
+          a: 'a',
+          b: 'b'
+        },
+        {
+          a: 'b',
+          b: 'c',
+        },
+        {
+          a: 'a',
+          b: 'c',
+        }
+      ],
+    }
+  },
   components: {
-    HelloWorld
+    DiffTree,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
